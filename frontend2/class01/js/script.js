e009f298ba154b12024072c436ccbeb9e0e23997
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-02 15:14:59 -0300

    first frontend2 class

---
+++ frontend2/class01/js/script.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!");
+
+window.document.body.style.background = "yellowhellnm";
+
+let input_data = window.prompt('Type your name!');
+
+if (input_data == 'Vinicius') {
+    window.alert('Whats up, Vini!');
+}
+else {
+    window.alert(`Hello, ${input_data}`);
+}
+
+let array = [0, 1, 2, 3, 4, 5, 6];
+
+const sum = array[0] + array[2];
+ 
+document.write(sum);
