9ede534b8a0fc8d6bac0125ee59391cecedfc941
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-03 16:44:42 -0300

    calc function added

--- frontend2/class02/class_assignment/calculator/app.js
+++ frontend2/class02/class_assignment/calculator/app.js
@@ -1,19 +1,59 @@
-let option = window.prompt('sum or subtraction? (s/ss)');
+function calc(){
 
-if (option.toLowerCase() == 's') {
-    let firstNum = Number(window.prompt('Type your first number'));
-    let secondNum = Number(window.prompt('Type your second number')); 
-    let sum = firstNum + secondNum;
-    alert(`${firstNum} + ${secondNum} = ${sum}`);
-}
-else if (option.toLowerCase() == 'ss') {
-    let firstNum = Number(window.prompt('Type your first number'));
-    let secondNum = Number(window.prompt('Type your second number')); 
-    let sum = firstNum - secondNum;
-    alert(sum);
-}
-else {
-    alert('Please, type a valid input.')
+    let option = window.prompt('Sum or Subtraction? (+ or -)');
+    
+    if (option.toLowerCase() == '+') {
+
+        let firstNum = Number(window.prompt('Type your first number'));
+        
+        if(typeof(firstNum) != Number){
+            alert('Please, type a number')
+            calc(); 
+        }
+
+        let secondNum = Number(window.prompt('Type your second number')); 
+        
+        if(isValid(secondNum) == false){
+            alert('Please, type a number')
+            calc(); 
+        }
+
+        let sum = firstNum + secondNum;
+        alert(`${firstNum} + ${secondNum} = ${sum}`);
+        let choice = window.prompt('Do you want to do it again? (y/n)')
+        if(choice.toLowerCase() == 'y'){
+            calc();
+        }
+
+    }
+    else if (option.toLowerCase() == '-') {
+
+        let firstNum = Number(window.prompt('Type your first number'));
+        
+        if(typeof(firstNum) != Number){
+            alert('Please, type a number')
+            calc(); 
+        }
+
+        let secondNum = Number(window.prompt('Type your second number')); 
+
+        if(typeof(secondNum) != Number){
+            alert('Please, type a number')
+            calc(); 
+        }
+        let sum = firstNum - secondNum;
+        alert(`${firstNum} - ${secondNum} = ${sum}`);
+        let choice = window.prompt('Do you want to do it again? (y/n)')
+        if(choice.toLowerCase() == 'y'){
+            calc();
+        }
+    }
+    else {
+        alert('Please, type a valid input.')
+        calc();
+    }
+
 }
 
+calc();
 
