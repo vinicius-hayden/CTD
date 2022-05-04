e079c1c583916d915093adc29ad0016c7a8c668f
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-04 09:31:11 -0300

    function loop

--- frontend2/class02/class_assignment/calculator/app.js
+++ frontend2/class02/class_assignment/calculator/app.js
@@ -1,19 +1,19 @@
-function calc(){
+function calc() {
 
     let option = window.prompt('Sum or Subtraction? (+ or -)');
     
-    if (option.toLowerCase() == '+') {
+    if (option == '+') {
 
         let firstNum = Number(window.prompt('Type your first number'));
         
-        if(typeof(firstNum) != Number){
+        if(typeof(firstNum) != Number) {
             alert('Please, type a number')
             calc(); 
         }
 
         let secondNum = Number(window.prompt('Type your second number')); 
         
-        if(isValid(secondNum) == false){
+        if(isValid(secondNum) == false) {
             alert('Please, type a number')
             calc(); 
         }
@@ -21,30 +21,30 @@ function calc(){
         let sum = firstNum + secondNum;
         alert(`${firstNum} + ${secondNum} = ${sum}`);
         let choice = window.prompt('Do you want to do it again? (y/n)')
-        if(choice.toLowerCase() == 'y'){
+        if(choice.toLowerCase() == 'y') {
             calc();
         }
 
     }
-    else if (option.toLowerCase() == '-') {
+    else if (option == '-') {
 
         let firstNum = Number(window.prompt('Type your first number'));
         
-        if(typeof(firstNum) != Number){
+        if(typeof(firstNum) != Number) {
             alert('Please, type a number')
             calc(); 
         }
 
         let secondNum = Number(window.prompt('Type your second number')); 
 
-        if(typeof(secondNum) != Number){
+        if(typeof(secondNum) != Number) {
             alert('Please, type a number')
             calc(); 
         }
         let sum = firstNum - secondNum;
         alert(`${firstNum} - ${secondNum} = ${sum}`);
         let choice = window.prompt('Do you want to do it again? (y/n)')
-        if(choice.toLowerCase() == 'y'){
+        if(choice.toLowerCase() == 'y') {
             calc();
         }
     }
