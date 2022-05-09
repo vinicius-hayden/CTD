c4be2ccdb9b78a15827b34ec8d445c5c9bc55f88
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-09 10:18:13 -0300

    UML

--- object-oriented-programming/class04/src/Program.java
+++ object-oriented-programming/class04/src/Program.java
@@ -4,9 +4,13 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Veterinarian vet1 = new Veterinarian("Robert", "Winkler", "CRV-12554");
-        Veterinarian vet2 = new Veterinarian();
+        Veterinarian vet1 = new Veterinarian();
+        Veterinarian vet2 = new Veterinarian("Robert", "Winkler", "CRV-12554", 3600);
 
+        vet1.name = "Claire";
+        vet1.lastname = "Thompson";
+        vet1.register = "CRV-54321";
+        vet1.salary = 2600.50;
     }
 
 }
--- object-oriented-programming/class04/src/entities/Veterinarian.java
+++ object-oriented-programming/class04/src/entities/Veterinarian.java
@@ -7,14 +7,17 @@ public class Veterinarian {
     public String lastname;
     public String register;
 
+    public double salary;
+
     //constructor
     public Veterinarian() {
     }
     //arguments constructor
-    public Veterinarian(String name, String lastname, String register) {
+    public Veterinarian(String name, String lastname, String register, double salary) {
         this.name = name;
         this.lastname = lastname;
         this.register = register;
+        this.salary = salary;
     }
 
 }
