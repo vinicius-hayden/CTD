d2f3517be6f871d3ef5c695e954c064934daa117
vinicius-hayden <vinihaydenss@gmail.com>
2022-05-09 09:41:49 -0300

    object

--- object-oriented-programming/class04/src/entities/Veterinarian.java
+++ object-oriented-programming/class04/src/entities/Veterinarian.java
@@ -1,7 +1,20 @@
+package entities;
+
 public class Veterinarian {
 
+    //attributes
     public String name;
     public String lastname;
     public String register;
+
+    //constructor
+    public Veterinarian() {
+    }
+    //arguments constructor
+    public Veterinarian(String name, String lastname, String register) {
+        this.name = name;
+        this.lastname = lastname;
+        this.register = register;
+    }
 
 }
