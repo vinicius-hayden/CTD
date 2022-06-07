71a4f2549a8b93b9329516c01acb8bffa17a2cd5
vinicius-hayden <vinihaydenss@gmail.com>
2022-06-07 16:58:14 -0300

    testing 1 deleted

--- testing1/unit_test/Listas/_test_/util.test.js
+++
@@ -1,7 +0,0 @@
-const { generateText } = require('../util.js');
-
-test('age and name output', () => {
-    const text = generateText('Vinicius Hayden', 18);
-    expect(text).toBe('Vinicius Hayden (18 years old)');
-
-})
