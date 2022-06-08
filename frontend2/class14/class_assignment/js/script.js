df870876a5c2fb69f9b5d20241fee6323686143e
vinicius-hayden <vinihaydenss@gmail.com>
2022-06-08 14:24:37 -0300

    styling and function with sass

--- frontend2/class14/class_assignment/js/script.js
+++ frontend2/class14/class_assignment/js/script.js
@@ -3,20 +3,20 @@ const password = document.getElementById("pass");
 const telephone = document.getElementById("tel");
 const button = document.getElementById("button");
 const form = document.getElementById("form");
-const hobbiesNetflix = document.getElementById('hobbiesNetflix');
-const hobbiesSoccer = document.getElementById('hobbiesSoccer');
-const hobbiesTennis = document.getElementById('hobbiesTennis');
-const hobbiesGym = document.getElementById('hobbiesGym');
-const hobbiesVideoGame = document.getElementById('hobbiesVideoGame');
-const hobbiesCooking = document.getElementById('hobbiesCooking');
-const hobbiesProgramming = document.getElementById('hobbiesProgramming');
-const hobbiesDrawing = document.getElementById('hobbiesDrawing');
-const hobbiesPainting = document.getElementById('hobbiesPainting');
-const hobbiesGuitar = document.getElementById('hobbiesGuitar');
-const hobbiesKeyboard = document.getElementById('hobbiesKeyboard');
-const hobbiesSewing = document.getElementById('hobbiesSewing');
-const hobbiesReading = document.getElementById('hobbiesReading');
-const hobbiesWriting = document.getElementById('hobbiesWriting');
+const hobbiesNetflix = document.getElementById("hobbiesNetflix");
+const hobbiesSoccer = document.getElementById("hobbiesSoccer");
+const hobbiesTennis = document.getElementById("hobbiesTennis");
+const hobbiesGym = document.getElementById("hobbiesGym");
+const hobbiesVideoGame = document.getElementById("hobbiesVideoGame");
+const hobbiesCooking = document.getElementById("hobbiesCooking");
+const hobbiesProgramming = document.getElementById("hobbiesProgramming");
+const hobbiesDrawing = document.getElementById("hobbiesDrawing");
+const hobbiesPainting = document.getElementById("hobbiesPainting");
+const hobbiesGuitar = document.getElementById("hobbiesGuitar");
+const hobbiesKeyboard = document.getElementById("hobbiesKeyboard");
+const hobbiesSewing = document.getElementById("hobbiesSewing");
+const hobbiesReading = document.getElementById("hobbiesReading");
+const hobbiesWriting = document.getElementById("hobbiesWriting");
 
 function isValidName(name) {
   return /^[a-zA-Z]+(?:\s[a-zA-Z]+)+$/.test(name);
@@ -26,23 +26,23 @@ function checkUsername() {
   let errorElement = document.getElementById("errorName");
   let errorMessage = [];
 
-  if (username.value.trim() === '' || username.value == null) {
-    errorMessage.push('This field is required');
-    errorElement.classList.add('wrong');
+  if (username.value.trim() === "" || username.value == null) {
+    errorMessage.push("This field is required");
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     window.scrollTo(0, 0);
     return false;
-  }
-  else if (username.value.length > 150) {
-    errorMessage.push('This field should not exceed 150 characters');
-    errorElement.classList.add('wrong');
+  } else if (username.value.length > 150) {
+    errorMessage.push("This field should not exceed 150 characters");
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     window.scrollTo(0, 0);
     return false;
-  }
-  else if (!isValidName(username.value)) {
-    errorMessage.push('There should be at least two words, with one letter each and no numbers');
-    errorElement.classList.add('wrong');
+  } else if (!isValidName(username.value)) {
+    errorMessage.push(
+      "There should be at least two words, with one letter each and no numbers"
+    );
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     window.scrollTo(0, 0);
     return false;
@@ -55,21 +55,27 @@ function checkPassword() {
   let errorMessage = [];
 
   if (password.value.search(/[a-z]/) < 0) {
-    errorMessage.push("Your password must contain at least one lowercase letter.");
+    errorMessage.push(
+      "Your password must contain at least one lowercase letter."
+    );
   }
   if (password.value.search(/[A-Z]/) < 0) {
-    errorMessage.push("Your password must contain at least one uppercase letter.");
+    errorMessage.push(
+      "Your password must contain at least one uppercase letter."
+    );
   }
   if (password.value.search(/[0-9]/) < 0) {
-    errorMessage.push("Your password must contain at least one digit.")
+    errorMessage.push("Your password must contain at least one digit.");
   }
   if (password.value.search(/[\!\@\#\$\%\^\&\*\(\)\_\+\.\,\;\:\-]/) < 0) {
-    errorMessage.push("Your password must contain at least one special character.");
+    errorMessage.push(
+      "Your password must contain at least one special character."
+    );
   }
 
   if (errorMessage.length > 0) {
-    errorElement.innerHTML = errorMessage.join('<br>')
-    errorElement.classList.add('wrong');
+    errorElement.innerHTML = errorMessage.join("<br>");
+    errorElement.classList.add("wrong");
     window.scrollTo(0, 0);
     return false;
   }
@@ -85,53 +91,47 @@ function isChecked(checkbox) {
 }
 
 function checkHobbies() {
-  let errorElement = document.getElementById('errorHobbies');
-  let errorMessage = []; 
+  let errorElement = document.getElementById("errorHobbies");
+  let errorMessage = [];
 
   if (isChecked(hobbiesVideoGame) && isChecked(hobbiesCooking)) {
-    errorMessage.push('This combination is not allowed')
-    errorElement.classList.add('wrong');
+    errorMessage.push("This combination is not allowed");
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     window.scrollTo(0, 0);
     return false;
   }
   if (isChecked(hobbiesGuitar) && isChecked(hobbiesReading)) {
-    errorMessage.push('This combination is not allowed')
-    errorElement.classList.add('wrong');
+    errorMessage.push("This combination is not allowed");
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     window.scrollTo(0, 0);
     return false;
   }
   if (isChecked(hobbiesNetflix) && isChecked(hobbiesSewing)) {
-    errorMessage.push('This combination is not allowed');
-    errorElement.classList.add('wrong');
+    errorMessage.push("This combination is not allowed");
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     window.scrollTo(0, 0);
-    errorMessage.push('This combination is not allowed');
+    errorMessage.push("This combination is not allowed");
     return false;
   }
   return true;
 }
 
 function checkedHobby() {
-  let errorElement = document.getElementById('errorCountry');
+  let errorElement = document.getElementById("errorHobbies");
   let errorMessage = [];
-  let hobbies = document.getElementsByName('hobbies');
+  let hobbies = document.getElementsByName("hobbies");
   var checkedHobbies = [];
-
   hobbies.forEach((value, index) => {
-    index.addEventListener('change', () => {
-      if(value.checked) {
-        checkedHobbies.push(index)
-      } else{
-        console.log('Not checked!');
-      }
-    })
-  })
-
-  if (checkedHobbies.length > 4) {
-    errorMessage.push('Please, only check 4 boxes');
-    errorElement.classList.add('wrong');
+    if (value.checked) {
+      checkedHobbies.push(index);
+    }
+  });
+  if (checkedHobbies.length >= 5) {
+    errorMessage.push("Please, only check 4 boxes");
+    errorElement.classList.add("wrong");
     errorElement.innerText = errorMessage;
     return false;
   } else {
@@ -139,19 +139,56 @@ function checkedHobby() {
   }
 }
 
+function checkCountry() {
+  let errorElement = document.getElementById("errorCountry");
+  let errorMessage = [];
+  let brazil = document.getElementById("brazil");
+  if (brazil.checked) {
+    errorMessage.push(
+      "Sorry, currently we are not recruting wizards from Brazil."
+    );
+    errorElement.classList.add("wrong");
+    errorElement.innerText = errorMessage;
+    return false;
+  } else {
+    return true;
+  }
+}
 
 form.addEventListener("submit", (event) => {
   if (!checkUsername()) {
     event.preventDefault();
-  }
-  else if (!checkPassword()) {
-    console.log("HELLO")
+  } else if (!checkPassword()) {
+    event.preventDefault();
+  } else if (!checkHobbies()) {
+    event.preventDefault();
+  } else if (!checkedHobby()) {
+    event.preventDefault();
+  } else if (!checkCountry()) {
     event.preventDefault();
   }
-  else if (!checkHobbies()) {
-    event.preventDefault();
-  }
-  else if(!checkedHobby()) {
-    event.preventDefault();
-  }
+});
+
+window.addEventListener("load", () => {
+  Element.prototype.setAttributes = function (attrs) {
+    for (var idx in attrs) {
+      if ((idx === "styles" || idx === "style") &&typeof attrs[idx] === "object") {
+        for (var prop in attrs[idx]) {this.style[prop] = attrs[idx][prop];}
+      } else if (idx === "html") {
+        this.innerHTML = attrs[idx];
+      } else {
+        this.setAttribute(idx, attrs[idx]);
+      }
+    }
+  };
+
+  let div = document.createElement('div');
+  let img = document.createElement('img');
+  let h1 = document.querySelector('h1');
+  div.classList.add('logo');
+  img.setAttributes({
+  'src' : 'https://i.pinimg.com/originals/0a/41/0e/0a410e3a4f03610eed0dbbbcd2f0a3db.png',
+  'alt' : 'logo'});
+  div.appendChild(img);
+  h1.parentNode.insertBefore(div, h1);
 });
