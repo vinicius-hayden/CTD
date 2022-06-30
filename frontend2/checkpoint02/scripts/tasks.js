76f4f51bfb4b4226acf9546fd6077cead6778b84
vinicius-hayden <vinihaydenss@gmail.com>
2022-06-29 21:42:39 -0300

    rest api CRUD functionalities

--- frontend2/checkpoint02/scripts/tasks.js
+++ frontend2/checkpoint02/scripts/tasks.js
@@ -1,213 +1,272 @@
-const newTask = document.getElementById('novaTarefa')
-const newTaskForm = document.querySelector('.nova-tarefa')
-const tasks = document.getElementById('tasks')
-const doneTasks = document.getElementById('doneTasks')
+const newTask = document.getElementById("novaTarefa");
+const newTaskForm = document.querySelector(".nova-tarefa");
+const tasks = document.getElementById("tasks");
+const doneTasks = document.getElementById("doneTasks");
+const logout = document.getElementById("closeApp");
+const url = "https://ctd-todo-api.herokuapp.com/v1";
 
-const logout = document.getElementById('closeApp')
-
-if (localStorage.getItem('jwt') == null || localStorage.getItem('jwt') == '') {
-  alert('Você precisa estar logado para acessar essa página!')
-  window.location.href = '/index.html'
+function timedRedirect(location, time) {
+  setTimeout(() => {
+    window.location.href = `/${location}.html`;
+  }, time);
 }
 
-logout.addEventListener('click', () => {
-  localStorage.removeItem('jwt')
-  alert('Deslogado com sucesso!')
-  setTimeout(() => {
-    window.location.href = '/index.html'
-  }, 1000)
-})
+function setUserName() {
+  const target = document.getElementById("user-name");
+  fetch(`${url}/users/getMe`, {
+    method: "GET",
+    headers: {
+      Accept: "*/*, application/json, text/plain",
+      "Content-Type": "application/json",
+      authorization: localStorage.getItem("jwt").replace(/"/g, ""),
+    },
+  }).then((response) => {
+    response.json().then((data) => {
+      target.innerText = data.firstName + " " + data.lastName;
+    });
+  });
+}
 
-newTaskForm.addEventListener('submit', event => {
-  event.preventDefault()
+if (localStorage.getItem("jwt") == null || localStorage.getItem("jwt") == "") {
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "You must be logged in to view this page",
+  }).then((result) => {
+    timedRedirect("signup", 1);
+  });
+}
+
+logout.addEventListener("click", () => {
+  localStorage.removeItem("jwt");
+  Swal.fire({
+    icon: "success",
+    title: "Logout success",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+  timedRedirect("index", 1500);
+});
+
+newTaskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
   if (
-    newTask.value !== '' &&
+    newTask.value !== "" &&
     newTask.value !== null &&
     newTask.value.length >= 5
   ) {
-    fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
-      method: 'POST',
+    fetch(`${url}/tasks`, {
+      method: "POST",
       headers: {
-        Accept: '*/*, application/json, text/plain',
-        'Content-Type': 'application/json',
-        authorization: localStorage.getItem('jwt').replace(/"/g, '')
+        Accept: "*/*, application/json, text/plain",
+        "Content-Type": "application/json",
+        authorization: localStorage.getItem("jwt").replace(/"/g, ""),
       },
       body: JSON.stringify({
         description: `${newTask.value}`,
-        completed: 'false'
-      })
+        completed: "false",
+      }),
     })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          throw Error(res.statusText)
-        } else if ((res.status = 200)) {
-          refreshTasks()
-          newTask.value = ''
+          throw Error(res.statusText);
+        } else if (res.status == 201) {
+          newTask.value = "";
+        } else if (res.status == 400) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Some of required data is insufficient",
+          });
+        } else if (res.status == 401) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Looks like you are not logged in",
+          }).then(() => {
+            timedRedirect("index", 1);
+          });
+        } else if (res.status == 500) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Internal Error. Please contact admin or try again later",
+          });
         }
+        window.location.reload();
       })
-      .catch(response => {
-        console.log(response)
-      })
+      .catch((err) => {
+        console.error(err.statusText);
+      });
   } else {
-    alert('Task description must not be null.')
+    Swal.fire({
+      icon: "warning",
+      title: "Oops...",
+      text: "Task description must not be null or have at least 5 characters long",
+    }).then(() => {
+      newTask.value = "";
+    });
   }
-})
+});
 
 function refreshTasks() {
-  fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
-    method: 'GET',
+  fetch(`${url}/tasks`, {
+    method: "GET",
     headers: {
-      Accept: '*/*, application/json, text/plain',
-      'Content-Type': 'application/json',
-      authorization: localStorage.getItem('jwt').replace(/"/g, '')
-    }
+      Accept: "*/*, application/json, text/plain",
+      "Content-Type": "application/json",
+      authorization: localStorage.getItem("jwt").replace(/"/g, ""),
+    },
   })
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        throw Error(response)
+        throw Error(response);
       } else {
-        response.json().then(data => {
-          data.forEach(element => {
-            if (element.completed == false) {
-              let li = document.createElement('li')
-              li.classList.add('tarefa')
-              li.setAttribute('id', element.id)
-              let div = document.createElement('div')
-              div.classList.add('not-done')
-              div.addEventListener('click', () => {
-                fetch(
-                  `https://ctd-todo-api.herokuapp.com/v1/tasks/${div.parentNode.id}`,
-                  {
-                    method: 'PUT',
-                    mode: 'cors',
+        response.json().then((data) => {
+          if (!document.getElementById(data.id)) {
+            data.forEach((element) => {
+              if (element.completed == false) {
+                let li = document.createElement("li");
+                li.classList.add("tarefa");
+                li.setAttribute("id", element.id);
+                let div = document.createElement("div");
+                div.classList.add("not-done");
+                div.addEventListener("click", () => {
+                  fetch(`${url}/tasks/${div.parentNode.id}`, {
+                    method: "PUT",
+                    mode: "cors",
                     headers: {
-                      'Access-Control-Allow-Origin': '*',
-                      Accept: '*/*, application/json, text/plain',
-                      'Content-Type': 'application/json',
+                      "Access-Control-Allow-Origin": "*",
+                      Accept: "*/*, application/json, text/plain",
+                      "Content-Type": "application/json",
                       authorization: localStorage
-                        .getItem('jwt')
-                        .replace(/"/g, '')
+                        .getItem("jwt")
+                        .replace(/"/g, ""),
                     },
                     body: JSON.stringify({
                       description: `${element.description}`,
-                      completed: 'true'
-                    })
-                  }
-                ).then(resposta => {
-                  if (!resposta.ok) {
-                    throw Error(resposta)
-                  } else {
-                    resposta.json().then(dados => {
-                      div.classList.remove('not-done')
-                      pDate.remove()
-                      // div.remove()
-                      rightDiv = document.createElement('div')
-                      undoButton = document.createElement('i')
-                      // undoButton.classList.add('fa-solid')
-                      // undoButton.classList.add('fa-arrow-rotate-left')
-                      // rightDiv.insertAdjacentElement('beforeend', undoButton)
-
-                      doneTasks.appendChild(div.parentNode)
-                    })
-                  }
-                })
-              })
-              let altDiv = document.createElement('div')
-              let pDesc = document.createElement('p')
-              let pDate = document.createElement('p')
-              let description = document.createTextNode(element.description)
-              let timeStamp = document.createTextNode(element.createdAt)
-              pDesc.appendChild(description)
-              pDate.appendChild(timeStamp)
-              pDesc.classList.add('nome')
-              pDate.classList.add('timestamp')
-              altDiv.classList.add('descricao')
-              altDiv.insertAdjacentElement('afterbegin', pDesc)
-              altDiv.insertAdjacentElement('beforeend', pDate)
-              li.insertAdjacentElement('afterbegin', div)
-              li.insertAdjacentElement('beforeend', altDiv)
-              tasks.appendChild(li)
-            } else if (element.completed == true) {
-              let doneLi = document.createElement('li')
-              doneLi.classList.add('tarefa')
-              doneLi.setAttribute('id', element.id)
-              let doneDiv = document.createElement('div')
-              doneDiv.classList.add('descricao')
-              pDesc = document.createElement('p')
-              description = document.createTextNode(element.description)
-              pDesc.appendChild(description)
-              pDesc.classList.add('nome')
-              doneDiv.insertAdjacentElement('afterbegin', pDesc)
-              doneLi.insertAdjacentElement('afterbegin', doneDiv)
-              let undoButton = document.createElement('i')
-              undoButton.classList.add('fa-solid')
-              undoButton.classList.add('fa-arrow-rotate-left')
-              let rightDiv = document.createElement('div')
-              doneDiv.insertAdjacentElement('beforeend', rightDiv)
-              let deleteButton = document.createElement('i')
-              deleteButton.classList.add('fa-solid')
-              deleteButton.classList.add('fa-trash-can')
-              rightDiv.insertAdjacentElement('beforeend', undoButton)
-              rightDiv.insertAdjacentElement('beforeend', deleteButton)
-              deleteButton.addEventListener('click', () => {
-                const deleteLi = deleteButton.parentNode.parentNode.parentNode
-                fetch(
-                  `https://ctd-todo-api.herokuapp.com/v1/tasks/${deleteLi.id}`,
-                  {
-                    method: 'DELETE',
-                    mode: 'cors',
-                    headers: {
-                      'Access-Control-Allow-Origin': '*',
-                      Accept: '*/*, application/json, text/plain',
-                      'Content-Type': 'application/json',
-                      authorization: localStorage
-                        .getItem('jwt')
-                        .replace(/"/g, '')
+                      completed: "true",
+                    }),
+                  }).then((resposta) => {
+                    if (!resposta.ok) {
+                      throw Error(resposta);
+                    } else {
+                      resposta.json().then(() => {
+                        window.location.reload();
+                      });
                     }
-                  }
-                )
-                deleteLi.remove()
-              })
-              undoButton.addEventListener('click', () => {
-                const undoneLi = undoButton.parentNode.parentNode.parentNode
-                fetch(
-                  `https://ctd-todo-api.herokuapp.com/v1/tasks/${undoneLi.id}`,
-                  {
-                    method: 'PUT',
-                    mode: 'cors',
-                    headers: {
-                      'Access-Control-Allow-Origin': '*',
-                      Accept: '*/*, application/json, text/plain',
-                      'Content-Type': 'application/json',
-                      authorization: localStorage
-                        .getItem('jwt')
-                        .replace(/"/g, '')
-                    },
-                    body: JSON.stringify({
-                      description: `${element.description}`,
-                      completed: 'false'
-                    })
-                  }
-                ).then(respost => {
-                  if (!respost.ok) {
-                    throw Error(respost)
-                  } else {
-                    respost.json().then(dadoss => {
-                      tasks.appendChild(undoneLi)
-                    })
-                  }
-                })
-              })
+                  });
+                });
+                let altDiv = document.createElement("div");
+                let pDesc = document.createElement("p");
+                let pDate = document.createElement("p");
+                let description = document.createTextNode(element.description);
+                let timeStamp = document.createTextNode(
+                  dayjs(element.createAt).format("DD/MM/YY-HH:mm")
+                );
 
-              doneTasks.appendChild(doneLi)
-            }
-          })
-        })
+                pDesc.appendChild(description);
+                pDate.appendChild(timeStamp);
+                pDesc.classList.add("nome");
+                pDate.classList.add("timestamp");
+                altDiv.classList.add("descricao");
+                altDiv.insertAdjacentElement("afterbegin", pDesc);
+                altDiv.insertAdjacentElement("beforeend", pDate);
+                li.insertAdjacentElement("afterbegin", div);
+                li.insertAdjacentElement("beforeend", altDiv);
+                tasks.appendChild(li);
+              } else if (element.completed == true) {
+                let doneLi = document.createElement("li");
+                doneLi.classList.add("tarefa");
+                doneLi.setAttribute("id", element.id);
+                let doneDiv = document.createElement("div");
+                doneDiv.classList.add("descricao");
+                pDesc = document.createElement("p");
+                description = document.createTextNode(element.description);
+                pDesc.appendChild(description);
+                pDesc.classList.add("nome");
+                doneDiv.insertAdjacentElement("afterbegin", pDesc);
+                doneLi.insertAdjacentElement("afterbegin", doneDiv);
+                let undoButton = document.createElement("i");
+                undoButton.classList.add("fa-solid");
+                undoButton.classList.add("fa-arrow-rotate-left");
+                let rightDiv = document.createElement("div");
+                doneDiv.insertAdjacentElement("beforeend", rightDiv);
+                let deleteButton = document.createElement("i");
+                deleteButton.classList.add("fa-solid");
+                deleteButton.classList.add("fa-trash-can");
+                rightDiv.insertAdjacentElement("beforeend", undoButton);
+                rightDiv.insertAdjacentElement("beforeend", deleteButton);
+                deleteButton.addEventListener("click", () => {
+                  const deleteLi =
+                    deleteButton.parentNode.parentNode.parentNode;
+                  fetch(
+                    `https://ctd-todo-api.herokuapp.com/v1/tasks/${deleteLi.id}`,
+                    {
+                      method: "DELETE",
+                      mode: "cors",
+                      headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        Accept: "*/*, application/json, text/plain",
+                        "Content-Type": "application/json",
+                        authorization: localStorage
+                          .getItem("jwt")
+                          .replace(/"/g, ""),
+                      },
+                    }
+                  );
+                  deleteLi.remove();
+                });
+                undoButton.addEventListener("click", () => {
+                  const undoneLi = undoButton.parentNode.parentNode.parentNode;
+                  fetch(
+                    `https://ctd-todo-api.herokuapp.com/v1/tasks/${undoneLi.id}`,
+                    {
+                      method: "PUT",
+                      mode: "cors",
+                      headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        Accept: "*/*, application/json, text/plain",
+                        "Content-Type": "application/json",
+                        authorization: localStorage
+                          .getItem("jwt")
+                          .replace(/"/g, ""),
+                      },
+                      body: JSON.stringify({
+                        description: `${element.description}`,
+                        completed: "false",
+                      }),
+                    }
+                  ).then((respost) => {
+                    if (!respost.ok) {
+                      throw Error(respost);
+                    } else {
+                      respost.json().then(() => {
+                        window.location.reload();
+                      });
+                    }
+                  });
+                });
+                doneTasks.appendChild(doneLi);
+              }
+            });
+          }
+        });
       }
     })
-    .catch(err => {
-      alert('No tasks found, create one or contact admin')
-    })
+    .catch((err) => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Could not retrieve data, please contact admin",
+        footer: `${err.status + ":" + err.statusText}`,
+      }).then(() => {
+        timedRedirect("index", 10);
+      });
+    });
 }
 
-refreshTasks()
+window.onload = () => {
+  setUserName();
+  refreshTasks();
+};
