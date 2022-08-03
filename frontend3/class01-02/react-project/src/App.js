fa626ea273b4b603b9f874ecaa9a25fb0fa2f939
vinicius-hayden <vinihaydenss@gmail.com>
2022-08-03 19:28:55 -0300

    vanilla javascript and react component

---
+++ frontend3/class01-02/react-project/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    // JSX JavaScript XML
+    <> 
+      <Header/>
+      <p>Hello, World!</p>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend3/class01-02/vanilla-javascript/index.js
@@ -0,0 +1,7 @@
+// SPA - Single Page Application
+let application = document.getElementById('root');
+let component = document.createElement('h1');
+let text = document.createTextNode('Hello, World!');
+
+application.appendChild(component);
+component.appendChild(text);
