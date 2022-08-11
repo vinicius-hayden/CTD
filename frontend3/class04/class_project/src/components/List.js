d6743c065077f6e51a93340d2b705a3ea9701a42
vinicius-hayden <vinihaydenss@gmail.com>
2022-08-10 22:12:50 -0300

    map, reduce, filter, find and react components

---
+++ frontend3/class04/class_project/src/components/List.js
@@ -0,0 +1,7 @@
+export default function List() { 
+  return (
+    <ul>
+      <li>Meu Item</li>
+    </ul>
+  );
+}
---
+++ frontend3/class04/class_project/src/pages/Home.js
@@ -0,0 +1,12 @@
+import List from "../components/List"
+
+export default function Home() {
+  // Your html gets in here 
+  // Your logic also gets in here 
+  return (
+    <>
+      <h1>My list</h1>
+      <List></List>
+    </>
+  ) 
+}
