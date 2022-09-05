644bf207e7ced460134fd8cc8dbf9030d1ebe377
vinicius-hayden <vinihaydenss@gmail.com>
2022-09-04 21:39:26 -0300

    useState

--- frontend3/checkpoint1/MyApresentation/src/components/Sidebar.jsx
+++ frontend3/checkpoint1/MyApresentation/src/components/Sidebar.jsx
@@ -1,23 +1,23 @@
 export default function Sidebar( {classState} ) {
   let currentState = null;
   if (classState) {
-    currentState = "l-sidebar--open";
+    currentState = "l-sidebar--open"; // l-sidebar--open
   } else {
-    currentState = "l-sidebar--close";
+    currentState = "l-sidebar--close"; //l-sidebar--close
   }
 
-  return <> 
-  <div className={currentState}>
-    <h1 className="l-logo">Vinicius Hayden</h1>
-    <nav className="c-sidebar">
-      <a className="c-nav__item" href="#sobre-mim">
-        sobre mim
-      </a>
-      <a className="c-nav__item" href="#contatos">
-        contatos
-      </a>
-    </nav>
-  </div>
-  </>
-
+  return (
+  <> 
+    <div className={currentState}>
+      <h1 className="l-logo">Vinicius Hayden</h1>
+      <nav className="c-sidebar">
+        <a className="c-nav__item" href="#sobre-mim">
+          sobre mim
+        </a>
+        <a className="c-nav__item" href="#contatos">
+          contatos
+        </a>
+      </nav>
+    </div>
+  </>);
 }
